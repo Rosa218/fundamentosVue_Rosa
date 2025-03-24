177b72bab83260add8c85e9fabf0810c91aacfce
Rosa218 <18620216@tlaxiaco.tecnm.mx>
2025-03-24 04:51:34 -0600

    Se creo el componente Card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row mt-4"> 
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-warning">Mas informacion</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-warning">Mas informacion</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-warning">Mas informacion</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 //metodos
+import Card from './Card.vue';
 </script>
 
 <template>
@@ -32,36 +33,9 @@
     </div>
     <!-- CIERRE DEL PRIMER ROW -->
     <!--INICIO DEL SEGUNDO ROW-->
-    <div class="row mt-4"> 
-      <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <button class="btn btn-warning">Mas informacion</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <button class="btn btn-warning">Mas informacion</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <button class="btn btn-warning">Mas informacion</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Card />
     <!--CIERRE DEL SEGUNDO ROW-->
+    
   </div>
 </template>
 
