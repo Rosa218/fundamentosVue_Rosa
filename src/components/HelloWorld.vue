e774850d963a8708608f700348d26d35c056dc6d
Rosa218 <18620216@tlaxiaco.tecnm.mx>
2025-03-24 04:28:22 -0600

    Se crea Card dentro de HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,24 +3,68 @@
 </script>
 
 <template>
-<!--Estructura html-->
+  <!--Estructura html-->
+  <div class="container">
 
-  <h1>Hola Mundo</h1>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7EIsawvTTStcv_kBcT3nsoxpV23t4r18T9Q&s" alt="Vue logo">
-  <p></p>
-  <button @click="saludar">Hola soy un boton</button>
+    <h1 class="text-center">MASCOTAS</h1>
+    <!--iNICIO DEL PRIMER ROW-->
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>GATOS</h6>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>PERROS</h6>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CIERRE DEL PRIMER ROW -->
+    <!--INICIO DEL SEGUNDO ROW-->
+    <div class="row mt-4"> 
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-warning">Mas informacion</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-warning">Mas informacion</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A&s" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-warning">Mas informacion</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--CIERRE DEL SEGUNDO ROW-->
+  </div>
 </template>
 
 <style scoped>
-h1{
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  font-size: 45px; 
-  color: red;
-}
-button{
-  border-radius: 15px;
-  background-color: #ffff;
-  color: black;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-}
+
 </style>
