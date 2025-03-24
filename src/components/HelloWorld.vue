5f9659f48e386664da2add833b3c8607c0869c4a
Rosa218 <18620216@tlaxiaco.tecnm.mx>
2025-03-24 01:47:35 -0600

    Subir el proyecto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+//metodos
+</script>
+
+<template>
+<!--Estructura html-->
+
+  <h1>Hola Mundo</h1>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7EIsawvTTStcv_kBcT3nsoxpV23t4r18T9Q&s" alt="Vue logo">
+  <p></p>
+  <button @click="saludar">Hola soy un boton</button>
+</template>
+
+<style scoped>
+h1{
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 45px; 
+  color: red;
+}
+button{
+  border-radius: 15px;
+  background-color: #ffff;
+  color: black;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
